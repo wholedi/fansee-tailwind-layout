450368e8bd90e9c9382a2e41a5b3804a567085d1
Diana <131402561+wholedi@users.noreply.github.com>
2023-07-14 22:33:11 +0300

    Background.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,48 @@
-/** @type {import('tailwindcss').Config} */
+/**@type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src//*.{html,js}"],
   theme: {
+    fontSize: {
+      sm: 'var(--font-size-sm)',
+      base: 'var(--font-size-base)',
+      lg: 'var(--font-size-lg)',
+      '4xl': 'var(--font-size-4xl)',
+    },
+    screens: {
+      'bp375': '375px',
+      'bp576': '576px',
+      'bp768': '768px',
+      'bp992': '992px',
+      'bp1200': '1200px',
+      'bp1440': '1440px',
+      'bp1920': '1920px',
+    },
     extend: {
       fontFamily: {
-        inter: ["Inter", "Avenir", "Helvetica", "Arial", "sans-serif"],
-      },
-      dropShadow: {
-        logo: "0 0 2em #646cffaa",
-        "logo-tw": "0 0 2em #38bdf8aa",
-      },
-      height: {
-        18: "4.5rem",
+        'primary': 'var(--font-primary)',
+        'secondary': 'var(--font-secondary)',
       },
       colors: {
-        "transparent-white": "rgba(255, 255, 255, 0.87)",
+        'accent': 'rgba(var(--accent), <alpha-value>)',
+
+        'background-primary': 'rgba(var(--background-primary), <alpha-value>)',
+        'background-secondary': 'rgba(var(--background-secondary), <alpha-value>)',
+
+        'text-primary': 'rgba(var(--text-primary), <alpha-value>)',
+        'text-secondary': 'rgba(var(--text-secondary), <alpha-value>)',
+        'text-highlighted': 'rgba(var(--text-highlighted), <alpha-value>)',
+
+        'input-icon-active': 'rgba(var(--input-icon-active), <alpha-value>)',
+        'input-icon-disabled': 'rgba(var(--input-icon-disabled), <alpha-value>)',
+        'input-border-active': 'rgba(var(--input-border-active), <alpha-value>)',
+        'input-border-disabled': 'rgba(var(--input-border-disabled), <alpha-value>)',
       },
+      backgroundImage: {
+        'gradient-radial-type1': 'var(--radial-gradient-type-1)',
+        'gradient-radial-type2': 'var(--radial-gradient-type-2)',
+      }
     },
   },
   plugins: [],
 };
+
